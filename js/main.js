eaf674f8fc6ea8f7b3707ada1458139aee9305ce
jhkim14 <jhkim79797728@gmail.com>
2021-06-25 10:48:09 +0900

    windmap api로 동작

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 import { HeatMap as HeatMap } from './heatmap.js';
 import { WindMap as WindMap } from './windmap.js'
 
-var southWest = L.latLng(30, 118),
-    northEast = L.latLng(44, 134),
-    bounds = L.latLngBounds(southWest, northEast)
 window.map = L.map('map')
     .setView([36, 128], 8)
-    .setMaxBounds(bounds);
+
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png').addTo(map);
 
 var heatmap = new HeatMap()
@@ -19,7 +16,3 @@ window.onload = function () {
 
 document.getElementById('showHeatMap').addEventListener('click', heatmap.toggleHeatMap)
 document.getElementById('playWind').addEventListener('click', windmap.toggleWindLayer)
-
-window.request_api = function(bounds){
-    
-}
