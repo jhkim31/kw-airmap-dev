153797909f303a8d5dd9015b4977878387aeb0b1
jhkim14 <jhkim79797728@gmail.com>
2021-06-25 17:10:16 +0900

    windmap 완성

--- js/main.js
+++ js/main.js
@@ -7,12 +7,134 @@ window.map = L.map('map')
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png').addTo(map);
 
 var heatmap = new HeatMap()
-var windmap = new WindMap();
+var windmap = new WindMap()
+var config = {}
+
+var wind_data = []
+var pm10_data = []
+var pm25_data = []
+var h_data = []
+var t_data = []
+
+function set_config() {
+    var a = L.point(map.getSize().x + 70, -70)
+    var initLat = map.containerPointToLatLng(a).lat;
+    var initLng = map.containerPointToLatLng(a).lng;
+    config.latGap = map.containerPointToLatLng(a).lat - map.getBounds()._northEast.lat;
+    config.lngGap = map.containerPointToLatLng(a).lng - map.getBounds()._northEast.lng;
+    config.maxlat = initLat;
+    config.maxlng = initLng;
+    config.gridX = Math.ceil(map.getSize().x / 70) + 2
+    config.gridY = Math.ceil(map.getSize().y / 70) + 2
+    config.minlng = initLng - config.lngGap * config.gridX
+    config.minlat = initLat - config.latGap * config.gridY
+}
 
 window.onload = function () {
-    heatmap.init();
-    windmap.init();
+    set_config()
+    var url = `http://localhost:4500/total?gridX=${config.gridX}&gridY=${config.gridY}&latGap=${config.latGap}&lngGap=${config.lngGap}&maxlat=${config.maxlat}&maxlng=${config.maxlng}&minlat=${config.minlat}&minlng=${config.minlng}`
+    fetch(url)
+        .then(e => e.json())
+        .then(d => {
+            var converting_data = convert_data_one_time(config, d)
+            wind_data = converting_data[0]
+            windmap.set_data(config, wind_data);
+        })
+    // heatmap.init();
 }
+
+map.on('move', () => {
+    windmap.stopAnim();
+})
+
+map.on('moveend', () => {
+    set_config()
+    var url = `http://localhost:4500/total?gridX=${config.gridX}&gridY=${config.gridY}&latGap=${config.latGap}&lngGap=${config.lngGap}&maxlat=${config.maxlat}&maxlng=${config.maxlng}&minlat=${config.minlat}&minlng=${config.minlng}`
+    console.log(url)
+    fetch(url)
+        .then(e => e.json())
+        .then(d => {
+            var converting_data = convert_data_one_time(config, d)
+            wind_data = converting_data[0]
+            windmap.set_data(config, wind_data);
+            windmap.startAnim()
+        })
+})
+map.on('zoomend', () => {
+    set_config()
+    var url = `http://localhost:4500/total?gridX=${config.gridX}&gridY=${config.gridY}&latGap=${config.latGap}&lngGap=${config.lngGap}&maxlat=${config.maxlat}&maxlng=${config.maxlng}&minlat=${config.minlat}&minlng=${config.minlng}`
+    console.log(url)
+    fetch(url)
+        .then(e => e.json())
+        .then(d => {
+            var converting_data = convert_data_one_time(config, d)
+            wind_data = converting_data[0]
+            windmap.set_data(config, wind_data);
+            windmap.startAnim()
+        })
+})
+
+function convert_data_one_time(config, json_data){
+    var return_data = []
+    var return_wind_data = []
+    var return_pm10_data = []
+    var return_pm25_data = []
+    var return_h_data = []
+    var return_t_data = []
+
+    //windData
+    json_data.forEach(a => {
+        var wind_tmp = []
+        a.forEach(b => {
+            wind_tmp.push([b.wx, b.wy])
+        })
+        return_wind_data.push(wind_tmp)
+    })
+    return_data.push(return_wind_data)
+
+    //pm10Data
+    json_data.forEach(a => {
+        var pm10_tmp = []
+        a.forEach(b => {
+            pm10_tmp.push(b.pm10)
+        })
+        return_pm10_data.push(pm10_tmp)
+    })
+    return_data.push(return_pm10_data)
+
+    //pm2.5Data
+    json_data.forEach(a => {
+        var pm25_tmp = []
+        a.forEach(b => {
+            pm25_tmp.push(b.pm25)
+        })
+        return_pm25_data.push(pm25_tmp)
+    })
+    return_data.push(return_pm25_data)
+
+    //humidityData
+    json_data.forEach(a => {
+        var h_tmp = []
+        a.forEach(b => {
+            h_tmp.push(b.h)
+        })
+        return_h_data.push(h_tmp)
+    })
+    return_data.push(return_h_data)
+
+    //temperatureData
+    json_data.forEach(a => {
+        var t_tmp = []
+        a.forEach(b => {
+            t_tmp.push(b.t)
+        })
+        return_t_data.push(t_tmp)
+    })
+    return_data.push(return_t_data)
+
+    return return_data
+}
+
 
 document.getElementById('showHeatMap').addEventListener('click', heatmap.toggleHeatMap)
 document.getElementById('playWind').addEventListener('click', windmap.toggleWindLayer)
--- js/windmap.js
+++ js/windmap.js
@@ -1,39 +1,22 @@
-import { windData as windData } from './data.js'
-
 var WindMap = function () {
+    //initialize
     var wind_config = {}
 
-    var cn = document.getElementById('windmap')         // 캔버스 객체
-    var c = cn.getContext('2d');                        // 캔버스
-    var a = []                                          // 바람 하나하나 객체의 배열
-    var cnx;                                            // 캔버스 width
-    var cny;                                            // 캔버스 height
-    window.grid = []                                       // 위도 경도에 따른 그리드 배열
-    var currentFrame = 0                                // 애니메이션의 현재 프레임
-    var animationId                                     // 애니메이션 아이디 (정지시 필요)
-    this.showWind = false
-    var windCount = 1000;                   //default
-    var showSpeed = 1;                       //default
-    var initLat
-    var initLng
+    var cn = document.getElementById('windmap')
+    cn.width = window.innerWidth
+    cn.height = window.innerHeight
+    var c = cn.getContext('2d');
+    var a = []
+    var cnx = cn.width - 1
+    var cny = cn.height - 1
+    var grid = []
+    var animationId
+    var showWind = false
+    var windCount = 1000;
+    var showSpeed = 1;
 
+    //initialize
 
-    //페이지 resize시 실행
-    window.onresize = () => {
-        cn.width = window.innerWidth
-        cn.height = window.innerHeight
-        cnx = cn.width - 1
-        cny = cn.height - 1
-    }
-
-    function build() {
-        a = [];
-        for (var i = 0; i < windCount; i++) {
-            buildobj(i)
-        }
-    }
-
-    //바람 객체 생성 (실제 인스턴스 생성)
     function buildobj(i) {
         var x = getRandomArbitrary(0, cnx)
         var y = getRandomArbitrary(0, cny)
@@ -41,47 +24,44 @@ var WindMap = function () {
         a[i] = new wind(x, y, map.containerPointToLatLng(point).lat, map.containerPointToLatLng(point).lng, i, animationId + getRandomArbitrary(60, 250))
     }
 
-    //특정 인덱스 바람 객체 삭제
     function removeObj(index) {
         buildobj(index)
         return 0;
     }
 
-    //바람 객체 클래스
     function wind(x, y, latitude, longitude, index, endFrame) {
-        this.index = index                              // 객체배열에서 인덱스(삭제시 필요)
-        this.x = x;                                     // 화면에서의 x 좌표
-        this.y = y;                                     // 화면에서의 y 좌표
-        this.latitude = latitude;                       // 지도에서의 위도
-        this.longitude = longitude;                     // 지도에서의 경도
-        this.endFrame = endFrame                              // 생성될 당시 프레임
-        //바람 객체 이동 함수 (현재 좌표의 벡터를 받아 그 벡터 방향으로 이동)    
+        this.index = index
+        this.x = x;
+        this.y = y;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.endFrame = endFrame
+
         this.windMove = function () {
-            if (this.x > cnx || this.y > cny || this.x < 0 || this.y < 0) {                 //만약 캔버스 범위를 벗어나면 삭제
+            if (this.x > cnx || this.y > cny || this.x < 0 || this.y < 0) {
                 return removeObj(this.index)
             } else {
-                if (animationId > this.endFrame) {             // 100프레임 (1 ~ 2초) 에서 250프레임 (4초정도) 지나면 삭제
+                if (animationId > this.endFrame) {
                     removeObj(this.index)
                 }
-                const ls = {                                                                // 이동을 위한 현재 위치 기록
+                const ls = {
                     x: this.x,
                     y: this.y
                 };
 
-                var nextVec = getVector(this.latitude, this.longitude)                          // 현재 좌표에서 벡터 계산
-                this.x = ls.x + nextVec[0] * showSpeed                                                  // 현재 좌표에서 벡터만큼 이동                                                                                                      
-                this.y = ls.y + nextVec[1] * showSpeed                                            // 현재 좌표에서 벡터만큼 이동                                                                                                      
+                var nextVec = getVector(this.latitude, this.longitude)
+                this.x = ls.x + nextVec[0] * showSpeed
+                this.y = ls.y + nextVec[1] * showSpeed
 
                 var point = L.point(this.x, this.y)
 
-                this.latitude = map.containerPointToLatLng(point).lat               // 이동한 만큼 다시 현재 위치 계산
+                this.latitude = map.containerPointToLatLng(point).lat
 
                 this.longitude = map.containerPointToLatLng(point).lng
 
                 c.beginPath();
                 c.lineWidth = 2.2;
                 c.strokeStyle = "black"
-
                 c.moveTo(ls.x, ls.y);
                 c.lineTo(this.x, this.y);
                 c.stroke();
@@ -91,19 +71,18 @@ var WindMap = function () {
     }
 
     function getVector(latitude, longitude) {
-        if (latitude <= wind_config.minlat || latitude >= wind_config.maxlat) return [0, 0, 0]             // 만약 위도 33 이하, 38 이상이면 1, -1 벡터 리턴
-        if (longitude <= wind_config.minlng || longitude >= wind_config.maxlng) return [0, 0, 0]         // 만약 경도 124 이하, 130 이상이면 1, -1 벡터 리턴
+        if (latitude <= wind_config.minlat || latitude >= wind_config.maxlat) return [0, 0, 0]
+        if (longitude <= wind_config.minlng || longitude >= wind_config.maxlng) return [0, 0, 0]
 
-        var gridn = selectGrid(latitude, longitude);                            // 현재 벡터에서 그리드 계산
+        var gridn = selectGrid(latitude, longitude);
         var g00 = grid[gridn[0]][gridn[1]]
         var g10 = grid[gridn[0]][gridn[1] + 1]
         var g01 = grid[gridn[0] + 1][gridn[1]]
         var g11 = grid[gridn[0] + 1][gridn[1] + 1]
 
-        return interpolate(latitude, longitude, g00, g10, g01, g11)      // 4 그리드로 보간값 구해서 리턴
+        return interpolate(latitude, longitude, g00, g10, g01, g11)
     }
 
-    //위도와 경도를 가지고 적절한 그리드 리턴 (경도 0.25 단위 , 위도 0.25 단위로 쪼개어져 있음.)
     function selectGrid(latitude, longitude) {
 
         var gridlat = Math.floor((wind_config.maxlat - latitude) / wind_config.latGap)
@@ -111,13 +90,10 @@ var WindMap = function () {
         return [gridlat, gridlng]
     }
 
-    //위도 경도. 그리드로 보간값 계산
     var interpolate = function (latitude, longitude, g00, g10, g01, g11) {
         var x = (longitude % wind_config.lngGap) * (1 / wind_config.lngGap)
-
         var d1 = x
         var d2 = 1 - x
-
         var x1_vector_x
         var x1_vector_y
         var x2_vector_x
@@ -131,112 +107,71 @@ var WindMap = function () {
             console.log("error", error)
             debugger;
         }
-
-
         var y = (latitude % wind_config.latGap) * (1 / wind_config.latGap)
         var d4 = y
         var d3 = 1 - y
-
         var result_vector_x = d3 * x2_vector_x + d4 * x1_vector_x
         var result_vector_y = d3 * x2_vector_y + d4 * x1_vector_y
         var result_vector_scale = Math.sqrt(result_vector_x * result_vector_x + result_vector_y * result_vector_y)
 
         var result_vector = [result_vector_x, result_vector_y, result_vector_scale]
-        return result_vector                //보간값 리턴
-    }
-    // 위.경도 그리드값 읽어오기
-    this.init = () => {
-        cn.width = window.innerWidth
-        cn.height = window.innerHeight
-        cnx = cn.width - 1
-        cny = cn.height - 1
-        readGrid()
-        build()
+        return result_vector
     }
 
-    function readGrid() {
-
-        var a = L.point(map.getSize().x + 70, -70)
-        initLat = map.containerPointToLatLng(a).lat;
-        initLng = map.containerPointToLatLng(a).lng;
-
-        wind_config.latGap = map.containerPointToLatLng(a).lat - map.getBounds()._northEast.lat;
-        wind_config.lngGap = map.containerPointToLatLng(a).lng - map.getBounds()._northEast.lng;
-        wind_config.maxlat = initLat;
-        wind_config.maxlng = initLng;
-        wind_config.gridX = Math.ceil(map.getSize().x / 70) + 2
-        wind_config.gridY = Math.ceil(map.getSize().y / 70) + 2
-        wind_config.minlng = initLng - wind_config.lngGap * wind_config.gridX
-        wind_config.minlat = initLat - wind_config.latGap * wind_config.gridY
-
-        console.log(wind_config)
-        var url = `http://localhost:4500/wind?gridX=${wind_config.gridX}&gridY=${wind_config.gridY}&latGap=${wind_config.latGap}&lngGap=${wind_config.lngGap}&maxlat=${wind_config.maxlat}&maxlng=${wind_config.maxlng}&minlat=${wind_config.minlat}&minlng=${wind_config.minlng}`
-        console.log(url)
-        fetch(url)
-            .then(e => e.json())
-            .then(d => {
-                console.log(d)
-                grid = d
-                
-            })
-    }
-
-    //min, max 랜덤값 리턴
     function getRandomArbitrary(min, max) {
         return Math.random() * (max - min) + min;
     }
 
-    // 애니메이션 생성
-    function anim() {
-        animationId = requestAnimationFrame(anim)
-        c.fillStyle = "rgba(255, 255, 255,0.3 )"
-        c.fillRect(0, 0, cn.width, cn.height);
-        a.forEach(function (e, i) {
-            e.windMove();
-        });
+    var anim = function () {
+        if (showWind) {
+            animationId = requestAnimationFrame(anim)
+            c.fillStyle = "rgba(255, 255, 255,0.3 )"
+            c.fillRect(0, 0, cn.width, cn.height);
+            a.forEach(function (e, i) {
+                e.windMove();
+            });
+        }
     }
 
-    //에니메이션 정지
-    function stopAnim() {
-        cancelAnimationFrame(animationId)
-        c.clearRect(0, 0, cn.width, cn.height);
+    this.startAnim = function () {
+        this.stopAnim()
+        build()
+        anim()
+    }
+
+    this.stopAnim = function () {
+        if (showWind) {
+            cancelAnimationFrame(animationId)
+            c.clearRect(0, 0, cn.width, cn.height);
+        }
     }
 
     this.toggleWindLayer = () => {
-        if (this.showWind) {
+        if (showWind) {
             a = []
-            stopAnim()
-            this.showWind = false
+            this.stopAnim()
+            showWind = false
         } else {
-            build();
-            anim()
-            this.showWind = true;
+            showWind = true;
+            this.startAnim()
         }
     }
 
-    map.on('move', () => {
-        if (this.showWind) {
-            stopAnim();
-        }
-    })
-
-    map.on('moveend', () => {
-        readGrid()
+    this.set_data = function (config, wind_data) {
+        cn.width = window.innerWidth
+        cn.height = window.innerHeight
+        cnx = cn.width - 1
+        cny = cn.height - 1
+        wind_config = config
+        grid = wind_data
         build()
-        console.log('moveend')
-        if (this.showWind) {
-            stopAnim();
-            anim()
-        }
-    })
-    map.on('zoomend', () => {
-        readGrid()
-        build()
-    })
+    }
 
-    map.on('click', (e) => {
-        console.log(getVector(e.latlng.lat, e.latlng.lng))
-    })
+    function build() {
+        for (var i = 0; i < windCount; i++) {
+            buildobj(i)
+        }
+    }
 }
 
 export { WindMap }
